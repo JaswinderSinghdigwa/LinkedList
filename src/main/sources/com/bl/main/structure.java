ae3b84c635f795680c4a2a13a510e77e9a07aa8e
Aman <you@example.com>
2021-09-26 23:21:15 +0530

    appending the data

--- src/main/sources/com/bl/main/structure.java
+++ src/main/sources/com/bl/main/structure.java
@@ -6,26 +6,13 @@ public class structure {
 	 public static void main(String[] args) {
 		    LinkedList llist = new LinkedList();
 
-		    llist.insertAtEnd(70);
-		    llist.insertMiddle(llist.head, 70);
-		    llist.insertAfter(llist.head.next, 2);
+		    llist.insertAtBeginning(56);
+		    llist.insertAfter(llist.head.next,30);
+		    llist.insertAfter(llist.head.next,70);
 
 		    System.out.println("Linked list: ");
 		    llist.printList();
 
-//		    System.out.println("\nAfter deleting an element: ");
-//		    llist.deleteNode(3);
-//		    llist.printList();
-//
-//		    System.out.println();
-//		    int item_to_find = 3;
-//		    if (llist.search(llist.head, item_to_find))
-//		      System.out.println(item_to_find + " is found");
-//		    else
-//		      System.out.println(item_to_find + " is not found");
-//
-//		    llist.sortLinkedList(llist.head);
-//		    System.out.println("\nSorted List: ");
-//		    llist.printList();
+//		  ;
 	}
 }
