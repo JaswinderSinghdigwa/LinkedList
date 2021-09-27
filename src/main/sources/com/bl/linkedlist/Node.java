79947a5c1a6297209fd6880f3fcae601ddbeace7
Aman <you@example.com>
2021-09-27 21:56:34 +0530

    refactor

--- src/main/sources/com/bl/linkedlist/Node.java
+++ src/main/sources/com/bl/linkedlist/Node.java
@@ -3,12 +3,12 @@ package com.bl.linkedlist;
 
 
 public class Node {
-	    int data;
+	    public int data;
 	    public Node next;
 
-	    Node(int d) {
-	      data = d;
-	      next = null;
-	    }
+//	    Node(int d) {
+//	      data = d;
+//	      next = null;
+//	    }
 	  
 }
---
+++ src/main/sources/com/bl/main/Stackmain.java
@@ -0,0 +1,27 @@
+package com.bl.main;
+
+import com.bl.stack.Stack;
+
+public class Stackmain {
+	 Stack stack = new Stack();
+     // insert Stack value
+     stack.push(70);
+     stack.push(30),
+     stack.push(56);
+
+     // print Stack elements
+     stack.display();
+
+     // print Top element of Stack
+//     System.out.printf("\nTop element is %d\n", obj.peek());
+
+     // Delete top element of Stack
+//     obj.pop();
+//
+//     // print Stack elements
+//     obj.display();
+
+     // print Top element of Stack
+//     System.out.printf("\nTop element is %d\n", obj.peek());
+ }
+}
